114ccef52bfcac1e1db8b965ec564774cf32726e
achille <simachille@yahoo.fr>
2021-08-30 13:44:31 +0200

    civilite

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ function App({ value }) {
         {
           civilite ?
             (
-              <>
+              <div className="py-3 text-center">
                 <h3 className="text-center"> {`Votre civilité est ${civilite === 'mr' ? 'Monsieur' : 'Madame'}`}</h3>
-
-              </>
+                <button type="button" className="btn btn-primary" onClick={()=>setCivilite(null)}>Reinitialiser</button>
+              </div>
             )
             : null
         }
 
-        <div>
-          <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+        <div className='text-center'>
+          <div className="btn-group btn-group-lg" role="group" aria-label="Basic radio toggle button group">
             <input onClick={updateCivilite} type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" value="mr" />
             <label className="btn btn-success" htmlFor="btnradio1">Monsieur</label>
 
