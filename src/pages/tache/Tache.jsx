381d8a620d15fa8054448441c6541d686910788f
achille <simachille@yahoo.fr>
2021-08-31 11:13:27 +0200

    Hook utilisateur

--- src/pages/tache/Tache.jsx
+++ src/pages/tache/Tache.jsx
@@ -1,17 +1,15 @@
 import { shape } from 'prop-types';
 import React from 'react';
 
-<<<<<<< HEAD
 const Tache = ({tache : {id, statut, titre, description, ti, tr}, handleClick}) => {
 
 
+  const {REACT_APP_API: dns} = process.env;
+
+  console.log(dns);
+
   return (
       <div className="tache card mb-3 text-dark" onClick={() => handleClick({id, statut})}>
-=======
-const Tache = ({tache : {titre, description, ti, tr}, item}) => {
-  return (
-      <div className="tache card mb-3 text-dark">
->>>>>>> 3dd781bfd227f38ecc3a9db03d633d2ea8bd7a1c
       <div className="card-body">
         <h3 className="card-title">{titre}</h3>
         <div className="card-texte">{description}</div>
--- src/pages/taches/Taches.jsx
+++ src/pages/taches/Taches.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import { useFetch } from '../../hooks';
 import Tache from '../tache/Tache';
 
-const Taches = ({taches, handleClick}) => {
+const Taches = () => {
+  
   const statusLabels = ['TO DO', 'WIP', 'TEST', 'DONE'];
+  const {data = [], errors} = useFetch({path: 'taches', options: {method: 'DELETE'}});
   return ( 
     <div className="container">
       <div className="row">
         {
+          errors ? <p>{errors.message}</p>   :(
           [1, 2 ,3, 4].map((item) => (
             <div className="col-sm-3" key={item}>
               <h2>{statusLabels[item - 1]}</h2>
               {
                 React.Children.toArray(
-                    taches 
+                    data   
                     .filter(tache => item === tache.statut)
-                    .map((tache) => <Tache tache={tache} item={item} handleClick={handleClick}/>)
+                    .map((tache) => <Tache tache={tache} item={item} handleClick={()=>{}}/>)
                 )
               }
             </div>
             
-          ))
+          )))
           
         }
       </div>
